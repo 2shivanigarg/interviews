2304bb7c778dc647533879036e52d417b215c265
Shivani Garg <garg.shivani02@gmail.com>
2019-12-16 18:09:54 +0530

    Correcting the starting amount as 1

--- company/google/CoinChange.java
+++ company/google/CoinChange.java
@@ -20,7 +20,7 @@ class Solution {
         Arrays.fill(dp, amount + 1);
         
         dp[0] = 0;
-        for(int i = 0; i <= amount; i++) {
+        for(int i = 1; i <= amount; i++) {
             for(int j = 0; j < coins.length; j++) {
                 if(coins[j] <= i) {
                     dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
