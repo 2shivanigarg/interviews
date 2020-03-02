5ce267f6cd13fea169a22eb72d85bd9de568cc17
Shivani Garg <garg.shivani02@gmail.com>
2020-03-02 15:36:31 +0530

    Adding logic details to single number III problem

--- leetcode/bit-manipulation/SingleNumberIII.java
+++ leetcode/bit-manipulation/SingleNumberIII.java
@@ -9,6 +9,31 @@
 //        The order of the result is not important. So in the above example, [5, 3] is also correct.
 //        Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
 
+/**
+ * Logic used:
+ *
+ * 1. Let’s say non-repeating elements are X, Y.
+ * 2. A XOR A = 0
+ * 3. XOR all the elements of array. This will cancel all the repeated elements.
+ * 4. Result will be X XOR Y, since only X and Y are not repeating.
+ * 5. 1 XOR 1 =  0 and 1 XOR 0 = 1, with this logic in the result of X XOR Y if any kth bit is set to 1
+ *    implies either kth bit is 1 either in X or in Y not in both.
+ * 6. Use the above step to divide all the elements in array into 2 groups,
+ *    one group which has the elements for which the kth bit is set to 1
+ *    and second group which has the elements for which the kth bit is 0.
+ * 7. Let’s have that kth bit as right most set bit
+ *    If N is a number then the expression below will give the right most set bit:
+ *    N & ~ (N - 1)
+ *
+ *    a. We know that N & ~N = 0
+ *    b. If we subtract 1 from the number, it will be subtracted from the right most set bit
+ *       and that bit will be become 0.
+ *    c. So if we negate the remaining number from step above then that bit will become 1.
+ *    d. Now N & ~(N-1) will make all the bits 0 but the right most set bit of a number.
+ * 8. Now we can claim that these two groups are responsible to produce X and Y.
+ * 9. Group 1: XOR all the elements whose kth bit is 1 will produce either X or Y.
+ * 10. Group 2: XOR all the elements whose kth bit is 0 will produce either X or Y.
+ */
 class Solution {
     public int[] singleNumber(int[] nums) {
         int[] result = new int[2];
