c74c6e98830be00403d09dcc613c4a32319d2749
Shivani Garg <garg.shivani02@gmail.com>
2020-03-02 12:49:40 +0530

    Adding Facebook problems

---
+++ company/facebook/TwoSum.java
@@ -0,0 +1,28 @@
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//
+//        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//        Example:
+//
+//        Given nums = [2, 7, 11, 15], target = 9,
+//
+//        Because nums[0] + nums[1] = 2 + 7 = 9,
+//        return [0, 1].
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if(map.containsKey(difference)) {
+                result[0] = i;
+                result[1] = map.get(difference);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
