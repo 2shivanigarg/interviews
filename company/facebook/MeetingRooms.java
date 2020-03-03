a0705e2ac198d402e4807005f48731ff351c36cf
Shivani Garg <garg.shivani02@gmail.com>
2020-03-03 10:51:47 +0530

    Did minor modification in MeetingRooms problem

--- company/facebook/MeetingRooms.java
+++ company/facebook/MeetingRooms.java
@@ -20,11 +20,7 @@
  */
 class Solution {
     public boolean canAttendMeetings(Interval[] intervals) {
-        Arrays.sort(intervals, new Comparator<Interval> {
-            public int compare(Interval a, Interval b) {
-                return a.start - b.start;
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> a.start - b.start);
 
         for(int i = 0; i < intervals.length; i++) {
             if(intervals[i].end > intervals[i + 1].start) {
